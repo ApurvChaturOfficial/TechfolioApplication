7e5b6e8f3624ca59a3602df0a212203fde760bbc
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2023-12-23 21:31:54 +0530

    Title and Icon Fixed

--- site/frontend/vite.config.js
+++ site/frontend/vite.config.js
@@ -9,10 +9,10 @@ export default defineConfig({
     react(),
     VitePWA({
       manifest:  {
-        "name": "Tadpole Organisation",
+        "name": "Techfolio Firm",
         "icons": [
           {
-            src: "Business/Tadpole/favicon_io/android-chrome-512x512.png",
+            src: "Business/Techfolio/favicon_io/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable"
